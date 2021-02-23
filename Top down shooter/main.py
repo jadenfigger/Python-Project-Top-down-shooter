1a609380e02f02aca907a026bd8f5e7538f25d37
jadenfigger <jadenfigger@gmail.com>
2021-02-23 14:04:36 -0700

    Add files via upload

--- Top down shooter/main.py
+++ Top down shooter/main.py
@@ -4,57 +4,79 @@ import sys
 WIDTH, HEIGHT = SCREEN_SIZE = (1024, 768)
 GAME_FPS = 60
 
-
-
-tiles = [[0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0],
-		 [0, 0, 0, 0, 0, 0]]
-
-
 class Player:
 	def __init__(self):
 		self.direction = pygame.math.Vector2(0, 1)
 		self.playerWidHei = pygame.math.Vector2()
 		self.pos = pygame.math.Vector2(WIDTH/2, HEIGHT/2)
-		self.vel = pygame.math.Vector2(0, 0)
-		self.acc = pygame.math.Vector2(0, 0)
 		self.moveSpeed = 3
-		self.maxSpeed = 10
 		self.rotationAngle = 0
 		self.rotationSpeed = 1
 
 		self.isIdle = True
 
-		self.path = "Characters/"
 		self.sprites = {"idle": [None] * 4, "up": [None] * 8, "down": [None] * 8, "right": [None] * 8, "left": [None] * 8}
 		self.walkingSpriteSheet = pygame.image.load("Characters/BODY_male_walking.png")
+		self.currentSprite = None
 
 	def createSprites(self):
 		# x: 16 y: 14, x: 49 y: 63  xWidth = 33 yHeight = 49  yBuffer = 15
 		for i in range(4):
 			self.sprites["idle"][i] = self.walkingSpriteSheet.subsurface((16, 14 + (i * 64), 33, 49))
 		self.playerWidHei = pygame.math.Vector2(self.sprites["idle"][i].get_size())
+		self.currentSprite = self.sprites["idle"][2]
 
 
 	def drawPlayer(self, surface):
-		# if (self.isIdle):
-		if (self.direction == (0, -1)):
-			surface.blit(self.sprites["idle"][0], (self.pos.x, self.pos.y, self.playerWidHei.x, self.playerWidHei.y))
-		elif (self.direction == (-1, 0)):
-			surface.blit(self.sprites["idle"][1], (self.pos.x, self.pos.y, self.playerWidHei.x, self.playerWidHei.y))
-		elif (self.direction == (0, 1)):
-			surface.blit(self.sprites["idle"][2], (self.pos.x, self.pos.y, self.playerWidHei.x, self.playerWidHei.y))
-		elif (self.direction == (1, 0)):
-			surface.blit(self.sprites["idle"][3], (self.pos.x, self.pos.y, self.playerWidHei.x, self.playerWidHei.y))
+		print(self.direction)
+		'''if (self.direction == (0, -self.moveSpeed)):
+			self.currentSprite = self.sprites["idle"][0]
+		elif (self.direction == (-self.moveSpeed, 0)):
+			self.currentSprite = self.sprites["idle"][1]
+		elif (self.direction == (0, self.moveSpeed)):
+			self.currentSprite = self.sprites["idle"][2]
+		elif (self.direction == (self.moveSpeed, 0)):
+			self.currentSprite = self.sprites["idle"][3]'''
+		
+		if self.direction.x < 0:
+			self.currentSprite = self.sprites['idle'][1]
+		elif self.direction.x > 0:
+			self.currentSprite = self.sprites['idle'][3]
+		elif self.direction.y < 0:
+			self.currentSprite = self.sprites['idle'][0]
+		elif self.direction.y > 0:
+			self.currentSprite = self.sprites['idle'][2]
 
+		surface.blit(self.currentSprite, (self.pos.x, self.pos.y, self.playerWidHei.x, self.playerWidHei.y))
 
 	def move(self): 
-		if (not self.isIdle):
-			self.pos += self.direction * self.moveSpeed
+		# w = 119
+		# a = 97
+		# s = 115
+		# d = 100
+		# space = 32
+		keys = pygame.key.get_pressed()
+		initialPosition = pygame.math.Vector2(self.pos.x, self.pos.y)
+
+		if (keys[119]):
+			self.pos.y += -1 * self.moveSpeed
+		elif (keys[115]):
+			self.pos.y += 1 * self.moveSpeed
+		if (keys[97]):
+			self.pos.x += -1 * self.moveSpeed
+		elif (keys[100]):
+			self.pos.x += 1 * self.moveSpeed
+
+
+		if (self.pos.y != initialPosition.y):
+			self.direction.y = self.pos.y - initialPosition.y
+		else: 
+			self.direction.y = 0
+
+		if (self.pos.x != initialPosition.x):
+			self.direction.x = self.pos.x - initialPosition.x
+		else:
+			self.direction.x = 0
 
 
 class Ground:
@@ -87,21 +109,10 @@ class GameController:
 
 
 	def eventLoop(self):
+
 		for event in pygame.event.get():
 			if (event.type == pygame.QUIT):
 				self.running = False
-			if (event.type == pygame.KEYDOWN):
-				if (event.key == pygame.K_w):
-					self.player.direction = pygame.math.Vector2(0, -1)
-				elif (event.key == pygame.K_s):
-					self.player.direction = pygame.math.Vector2(0, 1)
-				elif (event.key == pygame.K_a):
-					self.player.direction = pygame.math.Vector2(-1, 0)
-				elif (event.key == pygame.K_d):
-					self.player.direction = pygame.math.Vector2(1, 0)
-				self.player.isIdle = False
-			if (event.type == pygame.KEYUP):
-				self.player.isIdle = True
 
 
 
