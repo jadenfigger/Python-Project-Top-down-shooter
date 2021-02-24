d31d8a9b6bd554c95c5541c02eedf0aea9540e15
Lily <47424966+Joe-O@users.noreply.github.com>
2021-02-24 14:22:45 -0700

    Add files via upload

---
+++ Top down shooter/Scripts/levels.py
@@ -0,0 +1,128 @@
+# TODO handle module stuff
+
+_dt = 'dirt'
+_gs = 'grass'
+_rd = 'road'
+_sd = 'sand'
+_sl = 'soil'
+_se = 'stone'
+_wr = 'water'
+
+level0 = [
+    [_wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _wr,
+     _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr],
+    [_wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs,
+     _gs, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr],
+    [_wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _wr, _wr, _wr, _wr, _wr],
+    [_wr, _wr, _wr, _wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _wr, _wr, _wr, _wr],
+    [_wr, _wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _wr, _wr],
+    [_wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_wr, _wr, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _wr, _wr, _wr, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs]
+]
+
+level1 = [
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd, _gs,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs],
+    [_gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _rd, _rd, _rd, _rd, _rd,
+     _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs, _gs]
+]
+
+'''
+level# = [
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _],
+    [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _]
+]
+'''
+
+_levelList = [level0, level1]
+
+
+def getLevel(i):
+    return _levelList[i]
+
+# w32 h20
